6ab0f1a6550cdbab9ea64211b2e57596ba1b35ec
maksssutka <gifka12357@gmail.com>
2023-12-06 08:34:23 +0300

    bbf

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function toggleSearch() {
+    var searchBar = document.querySelector('.search-bar');
+    searchBar.style.display = (searchBar.style.display === 'none' || searchBar.style.display === '') ? 'block' : 'none';
+  }
+  
